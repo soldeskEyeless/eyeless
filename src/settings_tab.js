94cb337ea5d400ae45b5fea79fd6539ccd0e76e8
hhseungjin <sjin0118@gmail.com>
2023-06-21 12:17:08 +0900

    230621 dropdown 수정

--- src/settings_tab.js
+++ src/settings_tab.js
@@ -8,47 +8,58 @@ import {
     TouchableOpacity
 } from 'react-native';
 
-import { Picker } from '@react-native-picker/picker';
+import { SelectList } from 'react-native-dropdown-select-list'
+
+
+
+
 
 function TabSettingsScreen() {
-    const [selectedValue, setSelectedValue ] = useState("10 minutes");
-    const [selectedValue1, setSelectedValue1 ] = useState("10 fontsize");
-    const fontSize = parseInt(selectedValue1);
+    const [selected, setSelected] = React.useState("");
+    const [selected2, setSelected2] = React.useState("");
+
+    const data = [
+        {key:'1',value:'10분'},
+        {key:'2',value:'20분'},
+        {key:'3',value:'30분'},
+        {key:'4',value:'40분'},
+        {key:'4',value:'50분'},
+        {key:'4',value:'60분'},
+      ];
+ 
+    const data2 = [
+        {key:'1', value:'작게'},
+        {key:'2', value:'보통'},
+        {key:'3', value:'크게'},
+    ];
+  
 
     return(
         <View style={{padding: 15}}>
             <Text style={{textAlign: 'left', fontSize: 17}}>시간</Text>
             <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
               <Text style={{textAlign: 'left', fontSize: 18, color: '#000'}}>자동 새로고침 시간</Text>
-              <Picker
-                selectedValue={selectedValue}
-                style={{ height: 50, width: 160 }}
-                onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                dropdownIconColor={'#4E2A84'}
-              >
-                <Picker.Item label="10 mins" value="10" style={{fontSize: 18}}/>
-                <Picker.Item label="20 mins" value="20" style={{fontSize: 18}}/>
-                <Picker.Item label="30 mins" value="30" style={{fontSize: 18}}/>
-                <Picker.Item label="40 mins" value="40" style={{fontSize: 18}}/>
-                <Picker.Item label="50 mins" value="50" style={{fontSize: 18}}/>
-                <Picker.Item label="60 mins" value="60" style={{fontSize: 18}}/>
-                <Picker.Item label="70 mins" value="70" style={{fontSize: 18}}/>
-              </Picker>
+
+              <SelectList 
+                setSelected={setSelected} 
+                data={data} 
+                save="value"
+                defaultOption={{key:'3',value:'30분'}}   //default selected option
+                />
+
             </View>
             <View style={{ borderBottomColor: '#D6D6D6', borderBottomWidth: 1, marginBottom: 10 }} />
             <Text style={{textAlign: 'left', fontSize: 17, }}>글자</Text>
             <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
               <Text style={{textAlign: 'left', fontSize: 18, color: '#000'}}>글자 크기</Text>
-              <Picker
-                selectedValue={selectedValue1}
-                style={{ height: 70, width: 130 }}
-                onValueChange={(itemValue, itemIndex) => setSelectedValue1(itemValue)}
-                dropdownIconColor={'#4E2A84'}
-              >
-                <Picker.Item label="작게" value="15" style={{fontSize: 15}} />
-                <Picker.Item label="중간" value="20" style={{fontSize: 20}} />
-                <Picker.Item label="크게" value="25" style={{fontSize: 25}} />
-              </Picker>
+              
+              <SelectList 
+                setSelected={setSelected2} 
+                data={data2} 
+                save="value"
+                defaultOption={{key:'2', value:'보통'}}   //default selected option
+                />
+
             </View>
             <View style={{ borderBottomColor: '#D6D6D6', borderBottomWidth: 1, marginBottom: 10}} />
             <Text style={{textAlign: 'left', fontSize: 17}}>버전</Text>
